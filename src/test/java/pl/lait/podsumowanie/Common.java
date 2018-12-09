daf526bb5e8752bf52172cabc44e3f11a438ad13
adam <adam@192.168.0.101>
2018-12-09 13:31:06 +0100

    dodana instrukcja

--- src/test/java/pl/lait/podsumowanie/Common.java
+++ src/test/java/pl/lait/podsumowanie/Common.java
@@ -4,25 +4,39 @@ import static org.junit.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Common {
 	
 	static WebDriver driver;
 	
 	public static void goToMainPage() {
+		System.out.println("Uruchamiam przeglądarkę");
 		driver = Init.getDriver();
 	}
 	
 	public static void linkClick(String linkText) {
+		System.out.println("Klikam w link: "+linkText);
 		try {
 			driver.findElement(By.linkText(linkText)).click();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
+			System.out.println("Generuję print screen z błędem");
 			Init.printScr(driver);
 			assertTrue("Nie znalazłem linku: " +
 			linkText, false);
 		}
 	}
+	
+	public static void checkPageTitleAndStopTestIfWrong(String title) {
+		boolean checkStatus = driver.getTitle().equals(title);
+		assertTrue("Tytuł strony jest nieprawidłowy", checkStatus);
+	}
 
+	public static WebElement findInputByName(String name) {
+		return driver.findElement(By.name(name));
+	}
+	
+	
 }
--- src/test/java/pl/lait/podsumowanie/TopMenuTest.java
+++ src/test/java/pl/lait/podsumowanie/TopMenuTest.java
@@ -11,50 +11,78 @@ public class TopMenuTest extends Common{
 	
 	@Test
 	public void topMenu() {
-		String testName = Thread.currentThread().getStackTrace()[1].getMethodName();
+		//wywołaj metodę z klasy Common - go to main page - otwiera przeglądarkę i przechodzi do seleniumhq.org
 		goToMainPage();
-		
-		//Init.printScr(driver, "TopMenuTest", testName);
-		//driver.findElement(By.linkText("About")).click();
+		//wywołaj metodę z klasy Common - sprawdzającą tytuł strony
+		checkPageTitleAndStopTestIfWrong("Selenium - Web Browser Automation");
+		//wywołaj metodę z klasy Common - linkClick()
 		linkClick("About");
-		Init.printScr(driver, "TopMenuTest", testName);
-		//Init.printScr(driver);
+		linkClick("Support");
+		linkClick("Documentation");
+		linkClick("Download");
+		linkClick("Projects");
+		//wywołaj metodę z klasy Common - findInputByname - zwraca webBelement więc można "po kropce" wywołać click/sendKeys itp
+		findInputByName("q").sendKeys("szukany tekst"); //wpisujemy tekst ale nie klikamy "submit" - tekst jest widoczny w okienku
+		
+		/*
+		 * sprawdzanie czy element znaleziony na stronie zawiera określony tekst
+		 */
+		
+		//1. umieszczam w zmiennej xPath z poszukiwanym elementem
+		String h2xpath = "//*[@id=\"mainContent\"]/h2";
+		//2. szukam elementu na stronie po XPATH
+		String h2text = driver.findElement(By.xpath(h2xpath)).getText();
+		//3. wyświetlam tekst ze znalezionego elementu
+		System.out.println("#####"+h2text);
+		//4. jeśli znaleziony element zawiera tekst "Selenium Projects"
+		//to wyświetlam tekst "Znaleziono poprawny...."
+		if(h2text.equals("Selenium Projects")) {
+			System.out.println("Znaleziono popawny "
+					+ "komunikat na stronie");
+		}
+		
 		driver.quit();
 	}
 	
+	
+	// 
 	@Test
 	public void topMenu2() {
+		//1. otworz stronę
 		WebDriver driver = Init.getDriver();
-		
+		//2. pobierz tytuł strony
 		String title = driver.getTitle();
+		//3. wyświetl tytuł strony
 		System.out.println(title);
-		
+		//4. kliknij w link "Support"
 		driver.findElement(By.linkText("Support")).click();
+		//5. zrób printScreen
 		Init.printScr(driver);
-		
+		//6. pobierz tytuł strony po przejściu na zakładkę "Support"
 		title = driver.getTitle();
-		
+		//7. wyświetl nowy tytuł strony
 		System.out.println(title);
-		
+		//8. sprawdź czy tytuł strony jest równy "Getting Help" - jeśli nie - przerwij test
 		assertTrue("Page title is wrong!", title.equals("Getting Help"));
-
+		//9. zamknij przeglądarkę
 		driver.quit();
 	}
 	
 	@Test
 	public void topMenu2wrongTitle() {
+		//cały test jest identyczny jak poprzedni do linijki assertTrue...
 		WebDriver driver = Init.getDriver();
 		
 		String title = driver.getTitle();
 		System.out.println(title);
 		
 		driver.findElement(By.linkText("Support")).click();
-		Init.printScr(driver);
+		//Init.printScr(driver);
 		
 		title = driver.getTitle();
 		
 		System.out.println(title);
-		
+		//sprawdź czy tytuł strony to "Some wrong page title" - jeśli jest inny - przerwij test
 		assertTrue("Page title is wrong!", title.equals("Some wrong page title"));
 
 		driver.quit();
